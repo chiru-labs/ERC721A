291e81bdc2074ff811976285a694f4237f06d0ce
Jacob MacMillan <31115470+MrMcGoats@users.noreply.github.com>
2022-01-16 14:23:13 -0800

    Added hardhat

---
+++ scripts/deploy.ts
@@ -0,0 +1,30 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from "hardhat";
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const Contract = await ethers.getContractFactory("ERC721A");
+  const contract = await Greeter.deploy();
+
+  await contract.deployed();
+
+  console.log("ERC721A deployed to:", contract.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
