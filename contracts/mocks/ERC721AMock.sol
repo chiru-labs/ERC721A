777cd31fe98ed0af83a809ce8988fb99053b18b5
Dan Ξ <danandreist@gmail.com>
2022-01-25 20:00:09 +0200

    Update ERC721AMock.sol

--- contracts/mocks/ERC721AMock.sol
+++ contracts/mocks/ERC721AMock.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Creators: Chiru Labs
 
-pragma solidity ^0.8.11;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
