cda1a5a9c3f923850eebb6555ace6106919d9a1d
kurt willis <38631399+willisk@users.noreply.github.com>
2022-01-28 05:40:08 +0100

    add require quantity > 0 in _safeMint() (#27)
    * add require > 0 in _mint()
    
    * added test, prettier formatting
    
    Co-authored-by: willisk <kurtwillis@live.com>

--- contracts/ERC721A.sol
+++ contracts/ERC721A.sol
@@ -310,6 +310,7 @@ contract ERC721A is Context, ERC165, IERC721, IERC721Metadata, IERC721Enumerable
         // We know if the first token in the batch doesn't exist, the other ones don't as well, because of serial ordering.
         require(!_exists(startTokenId), 'ERC721A: token already minted');
         require(quantity <= maxBatchSize, 'ERC721A: quantity to mint too high');
+        require(quantity > 0, 'ERC721A: quantity must be greater 0');
 
         _beforeTokenTransfers(address(0), to, startTokenId, quantity);
 
