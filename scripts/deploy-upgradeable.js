bbeac7003f6af35934fdb05e3be366435a4ec6fc
Jacob <31115470+MrMcGoats@users.noreply.github.com>
2022-01-28 12:01:25 -0800

    Added deploy and upgrade scripts for upgradeable version

---
+++ scripts/deploy-upgradeable.js
@@ -0,0 +1,35 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers, upgrades } from "hardhat";
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const [owner] = await ethers.getSigners();
+
+  const Contract = await ethers.getContractFactory("ERC721AUpgradeable");
+  const contract = await upgrades.deployProxy(Contract, [
+    "ERC721A",
+    "COIN",
+    10
+  ]);
+  await contract.deployed();
+
+  console.log("ERC721AUpgradeable deployed to:", contract.address, "by", owner.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
