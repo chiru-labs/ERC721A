f9698d39860f57a9484df92b97c90f4c03f94054
naomsa <djbrucelee@protonmail.com>
2022-01-29 19:51:02 -0300

    added dotenv and gas reporter easier config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,15 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 require('@nomiclabs/hardhat-ethers');
-
-if (process.env.REPORT_GAS) {
-  require("hardhat-gas-reporter");
-}
-
+require('hardhat-gas-reporter');
+require('dotenv').config();
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.11",
+  solidity: '0.8.11',
   gasReporter: {
+    enabled: process.env.REPORT_GAS,
     currency: 'USD',
-  }
+    coinmarketcap: process.env.COINMARKETCAP_KEY || '',
+  },
 };
