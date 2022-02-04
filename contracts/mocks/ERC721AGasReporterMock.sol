d45e03d227e4d9b9e345edcb6864fa916cc05255
cygaar <97691933+cygaar@users.noreply.github.com>
2022-02-03 21:50:46 -0800

    Add scripts for explicitly testing gas usage (#70)
    * Add scripts for explicitly testing gas usage
    
    * Remove unused imports

---
+++ contracts/mocks/ERC721AGasReporterMock.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+// Creators: Chiru Labs
+
+pragma solidity ^0.8.0;
+
+import '../ERC721A.sol';
+
+contract ERC721AGasReporterMock is ERC721A {
+    constructor(string memory name_, string memory symbol_) ERC721A(name_, symbol_) {}
+
+    function safeMintOne(address to) public {
+        _safeMint(to, 1);
+    }
+
+    function mintOne(address to) public {
+        _mint(to, 1, '', false);
+    }
+
+    function safeMintTen(address to) public {
+        _safeMint(to, 10);
+    }
+
+    function mintTen(address to) public {
+        _mint(to, 10, '', false);
+    }
+}
