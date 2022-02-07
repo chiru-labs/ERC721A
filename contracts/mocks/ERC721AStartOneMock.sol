9e562e6f8c9eb6ea1485790c9bc58ba630a03993
Phil <Pczek@users.noreply.github.com>
2022-02-07 22:09:28 +0100

    creates copy of original test with startTokenId set to 1

---
+++ contracts/mocks/ERC721AStartOneMock.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+// Creators: Chiru Labs
+
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/utils/Strings.sol';
+import '../ERC721A.sol';
+
+contract ERC721AStartOneMock is ERC721A {
+    constructor(string memory name_, string memory symbol_) ERC721A(name_, symbol_) {}
+
+    function _startTokenId() internal pure override returns (uint256) {
+        return 1;
+    }
+
+    function numberMinted(address owner) public view returns (uint256) {
+        return _numberMinted(owner);
+    }
+
+    function baseURI() public view returns (string memory) {
+        return _baseURI();
+    }
+
+    function exists(uint256 tokenId) public view returns (bool) {
+        return _exists(tokenId);
+    }
+
+    function safeMint(address to, uint256 quantity) public {
+        _safeMint(to, quantity);
+    }
+
+    function safeMint(
+        address to,
+        uint256 quantity,
+        bytes memory _data
+    ) public {
+        _safeMint(to, quantity, _data);
+    }
+
+    function mint(
+        address to,
+        uint256 quantity,
+        bytes memory _data,
+        bool safe
+    ) public {
+        _mint(to, quantity, _data, safe);
+    }
+}
