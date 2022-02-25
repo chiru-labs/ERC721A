e16a79a5bc8382aef740cc843e3c4d04a3ff03d9
Phil <Pczek@users.noreply.github.com>
2022-02-25 15:44:33 +0100

    creates a ERC721ABurnable Mock with custom startTokenId and uses it in a parameterised test suite testing the burnable extension.

---
+++ contracts/mocks/ERC721ABurnableStartTokenIdMock.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// Creators: Chiru Labs
+
+pragma solidity ^0.8.4;
+
+import './ERC721ABurnableMock.sol';
+import './StartTokenIdHelper.sol';
+
+contract ERC721ABurnableStartTokenIdMock is StartTokenIdHelper, ERC721ABurnableMock {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 startTokenId_
+    ) StartTokenIdHelper(startTokenId_) ERC721ABurnableMock(name_, symbol_) {}
+
+    function _startTokenId() internal view override returns (uint256) {
+        return startTokenId;
+    }
+}
