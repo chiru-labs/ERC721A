de45be8e4ce48e39dfae023efa4fec566752f071
Johnny Shankman <johnnyshankman@users.noreply.github.com>
2022-03-27 18:20:59 -0400

    Unit tests

--- contracts/extensions/ERC721APausable.sol
+++ contracts/extensions/ERC721APausable.sol
@@ -7,6 +7,7 @@ import '../ERC721A.sol';
 import "@openzeppelin/contracts/security/Pausable.sol";
 
 error ContractPaused();
+
 /**
  * @dev ERC721A token with pausable token transfers, minting and burning.
  *
