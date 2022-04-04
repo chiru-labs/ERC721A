76348a25fb580004778e2b118956504c720e7f35
Vectorized <webby1111@hotmail.com>
2022-04-04 00:23:31 +0000

    Add queries functions + tests

--- contracts/extensions/ERC721AQueries.sol
+++ contracts/extensions/ERC721AQueries.sol
@@ -7,45 +7,150 @@ import '../ERC721A.sol';
 
 /**
  * @title ERC721A Queries
- * @dev ERC721A Helper functions for Low Cap (<= 10,000) totalSupply.
+ * @dev ERC721A subclass with convenience query functions.
  */
 abstract contract ERC721AQueries is ERC721A {
     /**
-     * @dev Returns the tokenIds of the address. O(totalSupply) in complexity.
+     * @dev Returns the `TokenOwnership` struct at `tokenId` without reverting.
+     *
+     * If the `tokenId` is out of bounds:
+     *   - `addr` = `address(0)`
+     *   - `startTimestamp` = `0`
+     *   - `burned` = `false`
+     *
+     * If the `tokenId` is burned:
+     *   - `addr` = `<Address of owner before token was burned>`
+     *   - `startTimestamp` = `<Timestamp when token was burned>`
+     *   - `burned = `true`
+     *
+     * Otherwise:
+     *   - `addr` = `<Address of owner>`
+     *   - `startTimestamp` = `<Timestamp of start of ownership>`
+     *   - `burned = `false`
      */
-    function tokensOfOwner(address owner) public view returns (uint256[] memory) {
-        uint256 holdingAmount = balanceOf(owner);
-        uint256 currSupply = _currentIndex;
-        uint256 tokenIdsIdx;
-        address currOwnershipAddr;
+    function rawOwnershipOf(uint256 tokenId) public view returns (TokenOwnership memory) {
+        if (_exists(tokenId)) {
+            return _ownershipOf(tokenId);
+        }
+        TokenOwnership memory ownership;
+        if (_ownerships[tokenId].burned) {
+            ownership = _ownerships[tokenId];
+        }
+        return ownership;
+    }
 
-        uint256[] memory list = new uint256[](holdingAmount);
-
+    /**
+     * @dev Returns an array of `TokenOwnership` structs at `tokenIds` in order.
+     * See {ERC721AQueries-rawOwnershipOf}
+     */
+    function rawOwnershipsOf(uint256[] memory tokenIds) external view returns (TokenOwnership[] memory) {
         unchecked {
-            for (uint256 i = _startTokenId(); i < currSupply; ++i) {
-                TokenOwnership memory ownership = _ownerships[i];
+            uint256 n = tokenIds.length;
+            TokenOwnership[] memory a = new TokenOwnership[](n);
+            for (uint256 i; i != n; ++i) {
+                a[i] = rawOwnershipOf(tokenIds[i]);
+            }
+            return a;
+        }
+    }
 
+    /**
+     * @dev Returns an array of token IDs owned by `owner`,
+     * in the range [`start`, `stop`)
+     * (i.e. `start <= tokenId < stop`).
+     *
+     * This function allows for tokens to be queried if the collection
+     * grows too big for a single call of {ERC721AQueries-tokensOfOwner}.
+     */
+    function tokensOfOwnerIn(
+        address owner,
+        uint256 start,
+        uint256 stop
+    ) external view returns (uint256[] memory) {
+        unchecked {
+            uint256 tokenIdsIdx;
+            uint256 stopLimit = _currentIndex;
+            // Set `start = max(start, _startTokenId())`.
+            if (start < _startTokenId()) {
+                start = _startTokenId();
+            }
+            // Set `stop = min(stop, _currentIndex)`.
+            if (stop > stopLimit) {
+                stop = stopLimit;
+            }
+            uint256 n = balanceOf(owner);
+            // Set `n = min(balanceOf(owner), stop - start)`,
+            // to cater for cases where `balanceOf(owner)` is too big.
+            if (start < stop) {
+                uint256 d = stop - start;
+                if (d < n) {
+                    n = d;
+                }
+            } else {
+                n = 0;
+            }
+            uint256[] memory a = new uint256[](n);
+            // We need to call `rawOwnershipOf(start)`, 
+            // because the slot at `start` may not be initialized.
+            TokenOwnership memory ownership = rawOwnershipOf(start);
+            address currOwnershipAddr;
+            // If the starting slot exists, initialize `currOwnershipAddr`.
+            if (ownership.addr != address(0) && !ownership.burned) {
+                currOwnershipAddr = ownership.addr;
+            }
+            for (uint256 i = start; i != stop && tokenIdsIdx != n; ++i) {
+                ownership = _ownerships[i];
                 if (ownership.burned) {
                     continue;
                 }
-
-                // Find out who owns this sequence
                 if (ownership.addr != address(0)) {
                     currOwnershipAddr = ownership.addr;
                 }
-
-                // Append tokens the last found owner owns in the sequence
                 if (currOwnershipAddr == owner) {
-                    list[tokenIdsIdx++] = i;
-                }
-
-                // All tokens have been found, we don't need to keep searching
-                if (tokenIdsIdx == holdingAmount) {
-                    break;
+                    a[tokenIdsIdx++] = i;
                 }
             }
+            // The assembly requires a non-empty array.
+            if (n != 0) {
+                // Downsize the array to fit.
+                assembly {
+                    mstore(a, tokenIdsIdx)
+                }
+            }
+            return a;
         }
+    }
 
-        return list;
+    /**
+     * @dev Returns an array of token IDs owned by `owner`.
+     *
+     * This function scans the ownership mapping and is O(totalSupply) in complexity.
+     * It is meant to be called off-chain.
+     *
+     * See {ERC721AQueries-tokensOfOwnerIn} for splitting the scan into
+     * multiple smaller scans if the collection is large enough to cause
+     * an out-of-gas error (10K pfp collections should be fine).
+     */
+    function tokensOfOwner(address owner) external view returns (uint256[] memory) {
+        unchecked {
+            uint256 tokenIdsIdx;
+            address currOwnershipAddr;
+            uint256 n = balanceOf(owner);
+            uint256[] memory a = new uint256[](n);
+            TokenOwnership memory ownership;
+            for (uint256 i = _startTokenId(); tokenIdsIdx != n; ++i) {
+                ownership = _ownerships[i];
+                if (ownership.burned) {
+                    continue;
+                }
+                if (ownership.addr != address(0)) {
+                    currOwnershipAddr = ownership.addr;
+                }
+                if (currOwnershipAddr == owner) {
+                    a[tokenIdsIdx++] = i;
+                }
+            }
+            return a;
+        }
     }
 }
--- test/extensions/ERC721AQueries.test.js
+++ test/extensions/ERC721AQueries.test.js
@@ -14,7 +14,65 @@ const createTestSuite = ({ contract, constructorArgs, setOwnersExplicit = false 
 
         this.startTokenId = this.erc721aQueries.startTokenId ? 
           (await this.erc721aQueries.startTokenId()).toNumber() : 0;
+
         offseted = (...arr) => arr.map((num) => BigNumber.from(this.startTokenId + num));
+      });
+
+      const expectRawOwnershipBurned = function (rawOwnership, address) {
+        expect(rawOwnership.burned).to.eql(true);
+        expect(rawOwnership.addr).to.eql(address);
+        expect(rawOwnership.startTimestamp).to.not.eql(BigNumber.from(0));
+      };
+
+      const expectRawOwnershipNotExists = function (rawOwnership) {
+        expect(rawOwnership.burned).to.eql(false);
+        expect(rawOwnership.addr).to.eql(ZERO_ADDRESS);
+        expect(rawOwnership.startTimestamp).to.eql(BigNumber.from(0));
+      };
+
+      const expectRawOwnershipExists = function (rawOwnership, address) {
+        expect(rawOwnership.burned).to.eql(false);
+        expect(rawOwnership.addr).to.eql(address);
+        expect(rawOwnership.startTimestamp).to.not.eql(BigNumber.from(0));
+      };
+
+      context('with no minted tokens', async function () {
+        beforeEach(async function () {
+          const [owner, addr1] = await ethers.getSigners();
+          this.owner = owner;
+          this.addr1 = addr1;
+        });
+
+        describe('tokensOfOwner', async function () {
+          it('returns empty array', async function () {
+            expect(await this.erc721aQueries.tokensOfOwner(this.owner.address)).to.eql([]);
+            expect(await this.erc721aQueries.tokensOfOwner(this.addr1.address)).to.eql([]);
+          });
+        });
+
+        describe('tokensOfOwnerIn', async function () {
+          it('returns empty array', async function () {
+            expect(await this.erc721aQueries.tokensOfOwnerIn(this.owner.address, 0, 9)).to.eql([]);
+            expect(await this.erc721aQueries.tokensOfOwnerIn(this.addr1.address, 0, 9)).to.eql([]);
+          });
+        });
+
+        describe('rawOwnershipOf', async function () {
+          it('returns empty struct', async function () {
+            expectRawOwnershipNotExists(await this.erc721aQueries.rawOwnershipOf(0));
+            expectRawOwnershipNotExists(await this.erc721aQueries.rawOwnershipOf(1));
+          });
+        });
+
+        describe('rawOwnershipsOf', async function () {
+          it('returns empty structs', async function () {
+            const tokenIds = [0, 1, 2, 3];
+            const rawOwnerships = await this.erc721aQueries.rawOwnershipsOf(tokenIds);
+            for (let i = 0; i < rawOwnerships.length; ++i) {
+              expectRawOwnershipNotExists(rawOwnerships[i]);
+            }
+          });
+        });
       });
 
       context('with minted tokens', async function () {
@@ -51,6 +109,9 @@ const createTestSuite = ({ contract, constructorArgs, setOwnersExplicit = false 
             tokens: offseted(6, 7, 8),
           };
 
+          this.lastTokenId = offseted(8)[0];
+          this.currentIndex = this.lastTokenId.add(1);
+
           this.mintOrder = [this.addr1, this.addr2, this.addr3, this.addr4, owner];
 
           for (const minter of this.mintOrder) {
@@ -72,14 +133,14 @@ const createTestSuite = ({ contract, constructorArgs, setOwnersExplicit = false 
         });
 
         describe('tokensOfOwner', async function () {
-          it('returns the correct token ids', async function () {
+          it('initial', async function () {
             for (const minter of this.mintOrder) {
               const tokens = await this.erc721aQueries.tokensOfOwner(minter.address);
               expect(tokens).to.eql(minter.expected.tokens);
             }
           });
 
-          it('returns the correct token ids after a transfer interferes with the normal logic', async function () {
+          it('after a transfer', async function () {
             // Break sequential order by transfering 7th token from owner to addr4
             const tokenIdToTransfer = offseted(7);
             await this.erc721aQueries.transferFrom(this.owner.address, this.addr4.address, tokenIdToTransfer[0]);
@@ -93,7 +154,7 @@ const createTestSuite = ({ contract, constructorArgs, setOwnersExplicit = false 
             expect(addr4Tokens).to.eql(tokenIdToTransfer);
           });
 
-          it('returns correct token ids with burned tokens', async function () {
+          it('after a burn', async function () {
             // Burn tokens
             const tokenIdToBurn = offseted(7);
             await this.erc721aQueries.burn(tokenIdToBurn[0]);
@@ -103,6 +164,124 @@ const createTestSuite = ({ contract, constructorArgs, setOwnersExplicit = false 
 
             // Verify the function can still read the correct token ids
             expect(ownerTokens).to.eql(offseted(6, 8));
+          });
+        });
+
+        describe('tokensOfOwnerIn', async function () {
+          const expectCorrect = async function (addr, start, stop) {
+            const expectedTokens = (await this.erc721aQueries.tokensOfOwner(addr))
+              .filter(x => BigNumber.from(start).lte(x) && BigNumber.from(stop).gt(x));
+            const tokens = await this.erc721aQueries.tokensOfOwnerIn(addr, start, stop);
+            expect(tokens).to.eql(expectedTokens);
+          };
+
+          const subTests = function (description, beforeEachFunction) {
+            describe(description, async function () {
+              it('all token ids', async function () {
+                await beforeEachFunction.call(this);
+                await expectCorrect.call(this, this.owner.address, this.startTokenId, this.currentIndex);
+                await expectCorrect.call(this, this.owner.address, this.startTokenId, this.currentIndex.add(1));
+              });
+
+              it('partial token ids', async function () {
+                await beforeEachFunction.call(this);
+                const ownerTokens = this.owner.expected.tokens;
+                const start = ownerTokens[0];
+                const stop = ownerTokens[ownerTokens.length - 1] + 1;
+                for (let o = 1; o <= ownerTokens.length; ++o) {
+                  // Start truncated.
+                  await expectCorrect.call(this, this.owner.address, start + o, stop);
+                  // End truncated.
+                  await expectCorrect.call(this, this.owner.address, start, stop - o);
+                  // Start and end truncated. This also tests for start + o > stop - o.
+                  await expectCorrect.call(this, this.owner.address, start + o, stop - o);
+                }
+                for (let o = 0; o <= this.currentIndex; ++o) {
+                  // Sliding window.
+                  await expectCorrect.call(this, this.owner.address, o, o + ownerTokens.length);
+                }
+              });
+            });
+          };
+
+          subTests('initial', async function () {});
+          
+          subTests('after a token tranfer', async function () {
+            await this.erc721aQueries.transferFrom(this.owner.address, this.addr4.address, offseted(7)[0]);
+          });
+
+          subTests('after a token burn', async function () {
+            await this.erc721aQueries.burn(offseted(7)[0]);
+          });
+        });
+
+        describe('rawOwnershipOf', async function () {
+          it('token exists', async function () {  
+            const tokenId = this.owner.expected.tokens[0];
+            const rawOwnership = await this.erc721aQueries.rawOwnershipOf(tokenId);
+            expectRawOwnershipExists(rawOwnership, this.owner.address);
+          });
+
+          it('after a token burn', async function () {
+            const tokenId = this.owner.expected.tokens[0];
+            await this.erc721aQueries.burn(tokenId);
+            const rawOwnership = await this.erc721aQueries.rawOwnershipOf(tokenId);
+            expectRawOwnershipBurned(rawOwnership, this.owner.address);
+          });
+
+          it('after a token transfer', async function () {
+            const tokenId = this.owner.expected.tokens[0];
+            await this.erc721aQueries.transferFrom(this.owner.address, this.addr4.address, tokenId);
+            const rawOwnership = await this.erc721aQueries.rawOwnershipOf(tokenId);
+            expectRawOwnershipExists(rawOwnership, this.addr4.address);
+          });
+
+          it('out of bounds', async function () {
+            const rawOwnership = await this.erc721aQueries.rawOwnershipOf(this.currentIndex);
+            expectRawOwnershipNotExists(rawOwnership);
+          });
+        });
+
+        describe('rawOwnershipsOf', async function () {
+          it('tokens exist', async function () {
+            const tokenIds = [].concat(this.owner.expected.tokens, this.addr3.expected.tokens);
+            const rawOwnerships = await this.erc721aQueries.rawOwnershipsOf(tokenIds);
+            for (let i = 0; i < tokenIds.length; ++i) {
+              const tokenId = await this.erc721aQueries.ownerOf(tokenIds[i]);
+              expectRawOwnershipExists(rawOwnerships[i], tokenId);
+            }
+          });
+
+          it('after a token burn', async function () {
+            const tokenIds = [].concat(this.owner.expected.tokens, this.addr3.expected.tokens);
+            await this.erc721aQueries.burn(tokenIds[0]);
+            const rawOwnerships = await this.erc721aQueries.rawOwnershipsOf(tokenIds);
+            expectRawOwnershipBurned(rawOwnerships[0], this.owner.address);
+            for (let i = 1; i < tokenIds.length; ++i) {
+              const tokenId = await this.erc721aQueries.ownerOf(tokenIds[i]);
+              expectRawOwnershipExists(rawOwnerships[i], tokenId);
+            }
+          });
+
+          it('after a token transfer', async function () {
+            const tokenIds = [].concat(this.owner.expected.tokens, this.addr3.expected.tokens);
+            await this.erc721aQueries.transferFrom(this.owner.address, this.addr4.address, tokenIds[0]);
+            const rawOwnerships = await this.erc721aQueries.rawOwnershipsOf(tokenIds);
+            expectRawOwnershipExists(rawOwnerships[0], this.addr4.address);
+            for (let i = 1; i < tokenIds.length; ++i) {
+              const tokenId = await this.erc721aQueries.ownerOf(tokenIds[i]);
+              expectRawOwnershipExists(rawOwnerships[i], tokenId);
+            }
+          });
+
+          it('out of bounds', async function () {
+            const tokenIds = [].concat([this.currentIndex], this.addr3.expected.tokens);
+            const rawOwnerships = await this.erc721aQueries.rawOwnershipsOf(tokenIds);
+            expectRawOwnershipNotExists(rawOwnerships[0]);
+            for (let i = 1; i < tokenIds.length; ++i) {
+              const tokenId = await this.erc721aQueries.ownerOf(tokenIds[i]);
+              expectRawOwnershipExists(rawOwnerships[i], tokenId);
+            }
           });
         });
       });
