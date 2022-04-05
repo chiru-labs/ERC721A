91bab34556037e201e608fcd40c1139fe6e9b406
cygaar <tmpftl1@gmail.com>
2022-04-04 21:54:39 -0700

    Update

--- test/GasUsage.test.js
+++ test/GasUsage.test.js
@@ -9,32 +9,32 @@ describe('ERC721A Gas Usage', function () {
   });
 
   context('mintOne', function () {
-    it('runs mintOne 50 times', async function () {
-      for (let i = 0; i < 50; i++) {
+    it('runs mintOne 100 times', async function () {
+      for (let i = 0; i < 100; i++) {
         await this.erc721a.mintOne(this.addr1.address);
       }
     });
   });
 
   context('safeMintOne', function () {
-    it('runs safeMintOne 50 times', async function () {
-      for (let i = 0; i < 50; i++) {
+    it('runs safeMintOne 100 times', async function () {
+      for (let i = 0; i < 100; i++) {
         await this.erc721a.safeMintOne(this.addr1.address);
       }
     });
   });
 
   context('mintTen', function () {
-    it('runs mintTen 50 times', async function () {
-      for (let i = 0; i < 50; i++) {
+    it('runs mintTen 100 times', async function () {
+      for (let i = 0; i < 100; i++) {
         await this.erc721a.mintTen(this.addr1.address);
       }
     });
   });
 
   context('safeMintTen', function () {
-    it('runs safeMintTen 50 times', async function () {
-      for (let i = 0; i < 50; i++) {
+    it('runs safeMintTen 100 times', async function () {
+      for (let i = 0; i < 100; i++) {
         await this.erc721a.safeMintTen(this.addr1.address);
       }
     });
