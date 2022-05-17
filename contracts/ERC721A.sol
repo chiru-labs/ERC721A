f7104f0e359c17cd9853dee9f528a5fdd956bb22
Vectorized <webby1111@hotmail.com>
2022-05-17 11:38:09 +0000

    Edit comments

--- contracts/ERC721A.sol
+++ contracts/ERC721A.sol
@@ -758,11 +758,10 @@ contract ERC721A is IERC721A {
      */
     function _toString(uint256 value) internal pure returns (string memory ptr) {
         assembly {
-            // The maximum value of a uint256 contains 78 digits, 
+            // The maximum value of a uint256 contains 78 digits (1 byte per digit), 
             // but we allocate 128 bytes to keep the free memory pointer 32-byte word aliged.
-            // 128 = 32 + 3 * 32. 
-            // We need a minimal of 3 32-byte words to store 78 digits, 
-            // and 1 more 32-byte word to store the length. 
+            // We will need 1 32-byte word to store the length, 
+            // and 3 32-byte words to store a maximum of 78 digits. Total: 32 + 3 * 32 = 128.
             ptr := add(mload(0x40), 128)
             // Update the free memory pointer to allocate.
             mstore(0x40, ptr)
@@ -772,26 +771,26 @@ contract ERC721A is IERC721A {
 
             // We write the string from rightmost digit to leftmost digit.
             // The following is essentially a do-while loop that also handles the zero case.
-            // Costs a bit more vs early return for the zero case, 
-            // but otherwise cheaper in terms of deployment and overall runtime costs.
+            // Costs a bit more than early returning for the zero case,
+            // but cheaper in terms of deployment and runtime costs.
             for { 
                 // Initialize and perform the first pass without check.
                 let temp := value
+                // Move the pointer 1 byte leftwards to point to an empty character slot.
                 ptr := sub(ptr, 1)
-                // Write the character to the pointer.
-                // 48 is the ASCII index of '0'.
+                // Write the character to the pointer. 48 is the ASCII index of '0'.
                 mstore8(ptr, add(48, mod(temp, 10)))
                 temp := div(temp, 10)
             } temp { 
                 // Keep dividing temp until zero.
-                temp := div(temp, 10) 
-            } { 
+                temp := div(temp, 10)
+            } { // Body of the for loop.
                 ptr := sub(ptr, 1)
                 mstore8(ptr, add(48, mod(temp, 10)))
             }
-
+            
             let length := sub(end, ptr)
-            // Move the pointer 32 bytes lower to make room for the length.
+            // Move the pointer 32 bytes leftwards to make room for the length.
             ptr := sub(ptr, 32)
             // Store the length.
             mstore(ptr, length)
