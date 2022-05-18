389b15e404bd6f1aef49010ac197423795a95cab
Vectorized <webby1111@hotmail.com>
2022-05-18 10:09:25 +0800

    Assembly for _toString (#283)

--- contracts/ERC721A.sol
+++ contracts/ERC721A.sol
@@ -773,26 +773,44 @@ contract ERC721A is IERC721A {
     /**
      * @dev Converts a `uint256` to its ASCII `string` decimal representation.
      */
-    function _toString(uint256 value) internal pure returns (string memory) {
-        // Inspired by OraclizeAPI's implementation - MIT licence
-        // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
-        unchecked {
-            if (value == 0) {
-                return '0';
+    function _toString(uint256 value) internal pure returns (string memory ptr) {
+        assembly {
+            // The maximum value of a uint256 contains 78 digits (1 byte per digit), 
+            // but we allocate 128 bytes to keep the free memory pointer 32-byte word aliged.
+            // We will need 1 32-byte word to store the length, 
+            // and 3 32-byte words to store a maximum of 78 digits. Total: 32 + 3 * 32 = 128.
+            ptr := add(mload(0x40), 128)
+            // Update the free memory pointer to allocate.
+            mstore(0x40, ptr)
+
+            // Cache the end of the memory to calculate the length later.
+            let end := ptr
+
+            // We write the string from the rightmost digit to the leftmost digit.
+            // The following is essentially a do-while loop that also handles the zero case.
+            // Costs a bit more than early returning for the zero case,
+            // but cheaper in terms of deployment and overall runtime costs.
+            for { 
+                // Initialize and perform the first pass without check.
+                let temp := value
+                // Move the pointer 1 byte leftwards to point to an empty character slot.
+                ptr := sub(ptr, 1)
+                // Write the character to the pointer. 48 is the ASCII index of '0'.
+                mstore8(ptr, add(48, mod(temp, 10)))
+                temp := div(temp, 10)
+            } temp { 
+                // Keep dividing `temp` until zero.
+                temp := div(temp, 10)
+            } { // Body of the for loop.
+                ptr := sub(ptr, 1)
+                mstore8(ptr, add(48, mod(temp, 10)))
             }
-            uint256 temp = value;
-            uint256 digits;
-            while (temp != 0) {
-                ++digits;
-                temp /= 10;
-            }
-            bytes memory buffer = new bytes(digits);
-            while (value != 0) {
-                --digits;
-                buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
-                value /= 10;
-            }
-            return string(buffer);
+            
+            let length := sub(end, ptr)
+            // Move the pointer 32 bytes leftwards to make room for the length.
+            ptr := sub(ptr, 32)
+            // Store the length.
+            mstore(ptr, length)
         }
     }
 }
