7770e17cea7f1fecd02408491f5d82848b17d3a1
naz <nazariyv@users.noreply.github.com>
2022-07-26 01:26:42 +0100

    Fix "error:0308010C:digital envelope routines::unsupported" (#388)

--- hardhat.config.js
+++ hardhat.config.js
@@ -29,3 +29,17 @@ module.exports = {
   },
   plugins: ['solidity-coverage'],
 };
+
+// The "ripemd160" algorithm is not available anymore in NodeJS 17+ (because of lib SSL 3).
+// The following code replaces it with "sha256" instead.
+
+const crypto = require('crypto');
+
+try {
+  crypto.createHash('ripemd160');
+} catch (e) {
+  const origCreateHash = crypto.createHash;
+  crypto.createHash = (alg, opts) => {
+    return origCreateHash(alg === 'ripemd160' ? 'sha256' : alg, opts);
+  };
+}
