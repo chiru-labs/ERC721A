50b8eb027a78187a3891ae7c3f5781650d0965cd
skarard <skarard@github.com>
2022-08-15 17:05:39 +0100

    Soulbound test

---
+++ contracts/mocks/ERC721ASoulboundMock.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+// ERC721A Contracts v4.2.2
+// Creators: Chiru Labs
+
+pragma solidity ^0.8.4;
+
+import '../extensions/ERC721ASoulbound.sol';
+
+contract ERC721ASoulboundMock is ERC721ASoulbound {
+    constructor(string memory name_, string memory symbol_) ERC721A(name_, symbol_) {}
+
+    function safeMint(address to, uint256 quantity) public {
+        _safeMint(to, quantity);
+    }
+}
---
+++ test/extensions/ERC721ASoulbound.test.js
@@ -0,0 +1,45 @@
+const { deployContract, getBlockTimestamp, mineBlockTimestamp, offsettedIndex } = require('../helpers.js');
+const { expect } = require('chai');
+const { constants } = require('@openzeppelin/test-helpers');
+const { ZERO_ADDRESS } = constants;
+
+const createTestSuite = ({ contract, constructorArgs }) =>
+  function () {
+    let offsetted;
+
+    context(`${contract}`, function () {
+      beforeEach(async function () {
+        this.erc721aSoulbound = await deployContract(contract, constructorArgs);
+
+        this.startTokenId = this.erc721aSoulbound.startTokenId
+          ? (await this.erc721aSoulbound.startTokenId()).toNumber()
+          : 0;
+
+        offsetted = (...arr) => offsettedIndex(this.startTokenId, arr);
+      });
+
+      beforeEach(async function () {
+        const [owner, addr1, addr2, spender] = await ethers.getSigners();
+        this.owner = owner;
+        this.addr1 = addr1;
+        this.addr2 = addr2;
+        this.spender = spender;
+        this.numTestTokens = 10;
+        this.burnedTokenId = 5;
+        this.notBurnedTokenId = 6;
+        await this.erc721aSoulbound['safeMint(address,uint256)'](this.addr1.address, this.numTestTokens);
+        await this.erc721aSoulbound.connect(this.addr1).burn(this.burnedTokenId);
+      });
+
+      it('cannot transfer token', async function () {
+        await expect(
+          this.erc721aSoulbound.connect(this.addr1).transferFrom(this.addr1.address, this.addr2.address, 6)
+        ).to.be.revertedWith('SoulboundTokenCannotBeTransferred');
+      });
+    });
+  };
+
+describe.only(
+  'ERC721ASoulbound',
+  createTestSuite({ contract: 'ERC721ASoulboundMock', constructorArgs: ['Azuki', 'AZUKI'] })
+);
