484564423d5d3dd9bc4f5a9ac21562466af9138a
Markus Osterlund / robriks <80549215+robriks@users.noreply.github.com>
2023-08-16 17:06:23 -0400

    Update ERC721A.sol
    'unintialized' -> 'uninitialized'

--- contracts/ERC721A.sol
+++ contracts/ERC721A.sol
@@ -360,7 +360,7 @@ contract ERC721A is IERC721A {
                 // Invariant:
                 // There will always be an initialized ownership slot
                 // (i.e. `ownership.addr != address(0) && ownership.burned == false`)
-                // before an unintialized ownership slot
+                // before an uninitialized ownership slot
                 // (i.e. `ownership.addr == address(0) && ownership.burned == false`)
                 // Hence, `tokenId` will not underflow.
                 //
